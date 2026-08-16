ced444299ae3ebc86c04b497d6f9f5996abf7635
sksinthu <sksinthu15@gmail.com>
2026-08-16 18:31:57 +0530

    Fix Next.js image remotePatterns for Unsplash and Cloudinary

--- next.config.ts
+++ next.config.ts
@@ -6,14 +6,20 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
       },
       {
-        protocol: 'https',
-        hostname: '**',
+        protocol: 'http',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
